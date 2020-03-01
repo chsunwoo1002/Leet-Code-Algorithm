e8610f979419b405726992010480764f255a02eb
BungBumBong <chsunwoo1002@gmail.com>
2020-02-29 20:12:27 -0800

    DFS, BFS, Graph

---
+++ python/207_Course_Schedule.py
@@ -0,0 +1,25 @@
+# 207 Course Schedule
+# https://leetcode.com/problems/course-schedule/
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = {n: [] for n in xrange(numCourses)}  # [0]
+
+        for n1, n2 in prerequisites:
+            graph[n1].append(n2)
+
+        for target_course in xrange(numCourses):  # [1]
+            stack = graph[target_course]
+            visited = set()
+            while stack:
+                course = stack.pop()
+                visited.add(course)
+                if course == target_course: return False
+                for ajc in graph[course]:
+                    if ajc not in visited:
+                        stack.append(ajc)
+        return True
